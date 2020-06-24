f491b4a6b70068a127d051ccb1fa5648776d6857
Michal Miškerník <michal@miskernik.sk>
2020-06-24 14:27:27 +0200

    Backend implementation

---
+++ backend/api/send.js
@@ -0,0 +1,43 @@
+export default (req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Only the POST method is allowed" });
+    return;
+  }
+
+  if (
+    req.body == null ||
+    req.body.emails == null ||
+    !Array.isArray(req.body.emails)
+  ) {
+    res.status(422).json({ error: "Invalid request body" });
+    return;
+  }
+
+  const failed = [];
+
+  for (const address of req.body.emails) {
+    if (typeof address !== "string" || !emailPattern.test(address)) {
+      res.status(422).json({ error: "Invalid email", email: address });
+      return;
+    }
+
+    if (Math.random() > 0.9) {
+      failed.push(address);
+    }
+  }
+
+  if (failed.length > 0) {
+    res
+      .status(500)
+      .json({
+        error: "Failed to send emails to some addresses",
+        emails: failed
+      });
+
+    return
+  }
+
+  res.end();
+};
+
+const emailPattern = /.+\@.+\..+/;
